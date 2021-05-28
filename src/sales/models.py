efb0e73a3a0f43fee397b1e1c3f5a820d73da538
Emmanuell69 <emmanuelthomas135@gmail.com>
2021-05-28 22:37:36 +0530

    second commit

--- src/sales/models.py
+++ src/sales/models.py
@@ -3,10 +3,10 @@ from customer.models import Customer
 from profiles.models import Profile
 from django.db import models
 from django.utils import timezone
-
+from .utils import generate_code
 # Create your models here.
 
-class Position(models.Models):
+class Position(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.FloatField(blank=True)
@@ -26,7 +26,7 @@ class Position(models.Models):
 class Sale(models.Model):
     transaction_id = models.CharField(max_length=12, blank=True)
     positions = models.ManyToManyField(Position)
-    total_price = models.FloatField(blank=True)
+    total_price = models.FloatField(blank=True, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     salesman = models.ForeignKey(Profile, on_delete=models.CASCADE)
     created = models.DateTimeField(blank=True)
@@ -34,5 +34,21 @@ class Sale(models.Model):
     
     def __str__(self):
         return f"sales for the amount of ${self.total_price}"
+    
+    def save(self, *args,**kwargs):
+        if self.transaction_id =="":
+            self.transaction_id =generate_code()
+        if self.created is None:
+            self.created = timezone.now()
+        return super().save(*args,**kwargs)
+    
+    def get_positions(self):
+        return self.positions.all()
 class CSV(models.Model):
-    pass
+    file_name = models.FileField(upload_to='csvs')
+    activated = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.file_name)
