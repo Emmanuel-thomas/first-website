563eb48378833d0bffcb9ed09a75e0aee7f2fa22
Emmanuell69 <emmanuelthomas135@gmail.com>
2021-06-28 21:25:05 +0530

    committed

--- src/sales/views.py
+++ src/sales/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Sale
 from .forms import SalesSearchForm
+import pandas as pd
 # Create your views here.
 
 def home_view(request):
@@ -11,6 +12,14 @@ def home_view(request):
         date_to = request.POST.get("date_to")
         chart_type = request.POST.get("chart_type")
         print(date_from, date_to, chart_type)
+
+        qs = Sale.objects.filter(created__date=date_from)
+        obj = Sale.objects.get(id=1)
+        print(qs)
+        print(obj)
+        print(qs.values())
+        print(qs.values_list())
+
     context = {
     
         'form':form,
