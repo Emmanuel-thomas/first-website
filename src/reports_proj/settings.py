35084918580b334afd0f5ec2ad4ca6d657facf28
Emmanuell69 <emmanuelthomas135@gmail.com>
2021-06-30 21:41:33 +0530

    javascript

--- src/reports_proj/settings.py
+++ src/reports_proj/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR /'static',
+    BASE_DIR /'sales'/'static',
+
 ]
 
 MEADIA_URL = '/media/'
--- src/sales/utils.py
+++ src/sales/utils.py
@@ -1,4 +1,6 @@
-import uuid
+import uuid, base64
+from io import BytesIO
+import matplotlib.pyplot as plt
 from customer.models import Customer
 from profiles.models import Profile
 
@@ -13,5 +15,38 @@ def get_salesman_from_id(val):
     return salesman.user.username
 
 def get_customer_from_id(val):
-    customer = Profile.objects.get(id=val)
-    return customer.user.username   
+    customer = Customer.objects.get(id=val)
+    return customer  
+
+def get_graph():
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+
+def get_chart(chart_type, data, **kwargs):
+    plt.switch_backend('AGG')
+    fig = plt.figure(figsize=(10,4))
+    if chart_type == '#1':
+        print("barchart")
+        plt.bar(data['transaction_id'], data['price'])
+    elif chart_type == '#2':
+        print("pie chart")
+        labels = kwargs.get('labels')
+        plt.pie(data=data, x='price',labels=labels)
+    elif chart_type == '#3':
+        print("line chart")
+        plt.plot(data['transaction_id'],data['price'])
+    else:
+        print("wrong input")
+    plt.tight_layout()
+    chart = get_graph
+    return chart
+
+    
+
